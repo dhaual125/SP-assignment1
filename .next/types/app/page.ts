ee84a89ccbffb1b68277a28484a234c2f7da6343
Dhraval <dhravaln@gmail.com>
2025-07-31 14:04:47 +0530

    second

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\AssigmnetNextJs\assignment\src\app\page.js
+// File: c:\AssigmnetNextJs\assignment\src\app\page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import './navbar.css';
 
 function Navbar() {
     //const usePathname = usePathname();
